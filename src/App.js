e2b0b329226257974cdfbf00c2e96fba35d3f4a6
George Bacalu <b.george1401@gmail.com>
2022-03-09 19:24:53 +0200

    Create all routing configurations and use http requests to send and retrieve data from the backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllQuotes from "./pages/AllQuotes";
+import SingleQuote from "./pages/SingleQuote";
+import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
-  return <div>Let's get started!</div>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <SingleQuote />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 };
 
 export default App;
--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,34 @@
-import QuoteItem from "./QuoteItem";
+import { useHistory, useLocation } from "react-router-dom";
 import classes from "./QuoteList.module.css";
+import QuoteItem from "./QuoteItem";
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((fisrtQuote, secondQuote) => {
+    if (ascending) return fisrtQuote.id > secondQuote.id ? 1 : -1;
+    else return fisrtQuote.id < secondQuote.id ? 1 : -1;
+  });
+};
 
 const QuoteList = props => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    history.push({ pathname: location.pathname, search: `?sort=${isSortingAscending ? "desc" : "asc"}` });
+  };
+
   return (
     <>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort {isSortingAscending ? "Descending" : "Ascending"}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map(quote => (
+        {sortedQuotes.map(quote => (
           <QuoteItem key={quote.id} id={quote.id} author={quote.author} text={quote.text} />
         ))}
       </ul>
